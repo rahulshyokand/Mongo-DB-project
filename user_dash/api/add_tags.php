0210382d52d826e87f441e6d145780aeb20d98a0
Rahul Shyokand <33034184+rahulshyokand@users.noreply.github.com>
2019-04-03 21:45:11 +0530

    Update add_tags.php

--- user_dash/api/add_tags.php
+++ user_dash/api/add_tags.php
@@ -4,27 +4,13 @@ require '../api/fetch_user.php' ;
 // file imported for mongo connection setup
 //require '../vendor/autoload.php'; // include Composer's autoloader
 //$i=0;
-
 if(isset($_POST['topics']))
 {
-	//$topics=$_POST['topics'];
-	//verify Data
-	// $con = new MongoDB\Client("mongodb://localhost:27017");
-	//$con = new MongoClient();
-	// Select Database
 	if($client){
-		// Selected in config.php File DataBase
-	  /********  $db = $->user_preference; *****/
-	    // Select Collection
-	    // $collection = $db->user_account;
-	    // $qry = array("email" => $email);
-	    // $result = $collection->findOne($qry);
-      //echo $result ;
       // Email ID Already Exists
 if($client){
     // Select Collection
 		$topics=$_POST['topics'];
-
     $tags_array = explode(",",$topics);
 	//echo (	print_r($tags_array) );
 
@@ -35,8 +21,8 @@ if($client){
     [ '$set' => [ 'tags' => $tags_array ]]
 );
 
-    //$result = $collection->updateOne( $filter =  ['email': $email],[$set:{'topics': $tags_array}]);
-    //echo "user inserted in collection with object id '{$result->getInsertedId()}'";
+//$result = $collection->updateOne( $filter =  ['email': $email],[$set:{'topics': $tags_array}]);
+//echo "user inserted in collection with object id '{$result->getInsertedId()}'";
   }
 if($result)
 {
